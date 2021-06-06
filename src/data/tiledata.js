455df681d15102acca93ce4b5242d763d6240127
PixelCook <zacharyigould@gmail.com>
2021-06-06 14:21:01 +0300

    update

--- src/data/tiledata.js
+++ src/data/tiledata.js
@@ -1,22 +1,22 @@
 const tileData = [
   {
-    img: "/images/house.png",
+    img: "/images/house.jpg",
     title: "House Party",
     about:
       "React, NodeJS, MongoDB, CSS, and HTML were all used to create a new party app that allows users to login with their Spotify account, access their playlists, add, delete and modify those playlists, play samples from searched songs and most importantly invite others to collaborate.",
     url: "https://house-party-frontend.herokuapp.com/",
-    git: "https://github.com/PixelCook/houseparty-frontend"
+    git: "https://github.com/PixelCook/houseparty-frontend",
   },
   {
-    img: "/images/stocks.png",
+    img: "/images/stocks.jpg",
     title: "Stock App",
     about:
       "Vanilla JS, CSS and HTML were used to access an api and present the user with topical news articles, current stock prices, and even allow the user to search stocks by name and convert their prices historically into an interactive graph.",
     url: "https://app.netlify.com/sites/gracious-benz-06b692/overview",
-    git: "https://github.com/PixelCook/StockAPI"
+    git: "https://github.com/PixelCook/StockAPI",
   },
   {
-    img: "/images/oculus.png",
+    img: "/images/oculus.jpg",
     title: "Oculus",
     about:
       "React, NodeJS, Python were used to create a eye disease diagnostic tool using machine learning. This was a hackathon project with a fullstack team and a data science team, we completed the project in 48 hours!",
@@ -24,11 +24,11 @@ const tileData = [
     git: "https://github.com/PixelCook/oculus-frontend",
   },
   {
-    img: "/images/scrum.png",
+    img: "/images/scrum.jpg",
     title: "SCRUM note app",
     about: "React based SCRUM note app",
     url: "https://zealous-jackson-1f44b3.netlify.app/",
-    git: "https://github.com/PixelCook/NoteApp"
+    git: "https://github.com/PixelCook/NoteApp",
   },
 ];
 
